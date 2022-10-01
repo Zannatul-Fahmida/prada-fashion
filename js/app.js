9358212a112546d76f6879bdeea439c667d04621
zannatul <jannatulfahmida15@gmail.com>
2022-10-01 19:08:32 +0600

    done

--- js/app.js
+++ js/app.js
@@ -24,3 +24,11 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+var closebtns = document.getElementsByClassName("close");
+var i;
+
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+    this.parentElement.style.display = 'none';
+  });
+}
