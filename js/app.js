a0162112b123803dcb4a8eacf549c2c0267c0dd2
zannatul <jannatulfahmida15@gmail.com>
2022-10-05 17:28:57 +0600

    some issues solved

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+//Close button
+var closebtn = document.getElementById("close");
+closebtn.addEventListener("click", function() {
+  closebtn.parentElement.style.display = 'none';
+});
+
+// Slider
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -24,11 +31,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
-var closebtns = document.getElementsByClassName("close");
-var i;
 
-for (i = 0; i < closebtns.length; i++) {
-  closebtns[i].addEventListener("click", function() {
-    this.parentElement.style.display = 'none';
-  });
-}
